3d142cf72f143bcad7fe780f5bb452f713e5b318
Anwesh43 <anweshthecool0@gmail.com>
2018-04-01 11:06:41 +0530

    adding DimensionsUtil

---
+++ incompletecircleview/src/main/java/ui/anwesome/com/incompletecircleview/DimensionsUtil.kt
@@ -0,0 +1,23 @@
+package ui.anwesome.com.incompletecircleview
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Point
+import android.hardware.display.DisplayManager
+import android.view.Display
+
+/**
+ * Created by anweshmishra on 01/04/18.
+ */
+
+class DimensionsUtil {
+    companion object {
+        fun getDimensions(activity : Activity) : Point {
+            val point : Point = Point()
+            val displayManager : DisplayManager =  activity.getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+            val display : Display = displayManager.getDisplay(0)
+            display.getRealSize(point)
+            return point 
+        }
+    }
+}
