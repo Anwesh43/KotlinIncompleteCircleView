732f7483ee1e0ded725170cf7417ce31fe17111c
Anwesh43 <anweshthecool0@gmail.com>
2018-04-01 10:05:17 +0530

    adding IncompleteCircleView

--- incompletecircleview/src/main/java/ui/anwesome/com/incompletecircleview/IncompleteCircleView.kt
+++ incompletecircleview/src/main/java/ui/anwesome/com/incompletecircleview/IncompleteCircleView.kt
@@ -4,5 +4,20 @@ package ui.anwesome.com.incompletecircleview
  * Created by anweshmishra on 01/04/18.
  */
 import android.graphics.*
-import android.content.*
+import android.content.Context
 import android.view.*
+
+class IncompleteCircleView (ctx : Context) : View(ctx) {
+    val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
