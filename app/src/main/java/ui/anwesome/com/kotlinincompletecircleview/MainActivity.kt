9829274af03a2d87e46bb14e3139a4d0663d5bbc
Anwesh43 <anweshthecool0@gmail.com>
2018-04-01 11:22:40 +0530

    adding IncompleteCircleView in activity

--- app/src/main/java/ui/anwesome/com/kotlinincompletecircleview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinincompletecircleview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinincompletecircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.incompletecircleview.IncompleteCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        IncompleteCircleView.create(this)
     }
 }
